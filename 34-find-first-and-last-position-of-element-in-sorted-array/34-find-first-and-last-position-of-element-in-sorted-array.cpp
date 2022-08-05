5fc6e85e50da6e1a47ddbdf628cee983eb71e68c
garima1214 <48708539+garima1214@users.noreply.github.com>
2022-08-05 15:54:21 +0530

    Time: 16 ms (36.19%), Space: 13.5 MB (98.17%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1,res=-1;
-        vector<int>ans;
-        while(start<=end)
+    vector<int> searchRange(vector<int>& a, int target) {
+    vector<int> p;   
+   int l=0;
+        int res=-1;
+   int e =a.size()-1;
+        int mid=0;
+        while(l<=e)
         {
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target)
-            {
-                res=mid;
-                end=mid-1;
+            mid= (l+e)/2;
+            if(a[mid]==target)
+            {   res=mid;
+              e=mid-1;
             }
-            else if(target<nums[mid])
-                end=mid-1;
             else
-                start=mid+1;   
+                if(target<a[mid])
+                    e=mid-1;
+            else
+                l=mid+1;
         }
-        ans.push_back(res);
-        start=0,end=nums.size()-1,res=-1;
-        while(start<=end)
+   p.push_back(res);
+         l=0;
+        res=-1;
+    e =a.size()-1;
+         mid=0;
+        while(l<=e)
         {
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target)
-            {
-                res=mid;
-                start=mid+1;
+            mid= (l+e)/2;
+            if(a[mid]==target)
+            {   res=mid;
+              l=mid+1;
             }
-            else if(target<nums[mid])
-                end=mid-1;
             else
-                start=mid+1;   
+                if(target<a[mid])
+                    e=mid-1;
+            else
+                l=mid+1;
         }
-        ans.push_back(res);
-        return ans;
+   p.push_back(res);
+       
+return p;
     }
 };
