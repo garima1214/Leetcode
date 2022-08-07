837c52a28156083042a965b977444ea296455019
garima1214 <48708539+garima1214@users.noreply.github.com>
2022-08-07 12:44:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.28%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+     long long  int l=0 ,e=x,ans=0;
+        while(l<=e)
+        {
+         long long  int mid =(l+e)/2;
+            if(mid*mid==x)
+                return mid;
+            if(mid*mid<x)
+            { l=mid+1;
+                ans =mid;
+            }
+            else
+                e=mid-1;
+           }
+        return ans;
     }
 };
