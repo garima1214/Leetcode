c32f262b018e5bb6a324513c1164c1b1d4f94e33
garima1214 <48708539+garima1214@users.noreply.github.com>
2022-09-17 22:36:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (79.92%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for (auto i :s)
+        {
+            if(i=='(' || i=='[' || i=='{' )
+                t.push(i);
+            else
+            {
+                if(t.empty() || (t.top()=='('&& i!=')') || (t.top()=='[' && i!=']') ||( t.top()=='{' && i!='}'))
+                    return false;
+            
+            else
+            t.pop();
+            }
+        }
+        
+        return t.empty();
+    }
+};
